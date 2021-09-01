bff1437780e64e5927bfeb0be72c8db3e2cfd560
nYCSTs <lucascandradex14@gmail.com>
2021-08-31 21:24:37 -0300

    Initial changes on HomePage and navbar

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,41 +1,43 @@
-import { Container, Row, Col } from 'react-bootstrap';
+import { Grid, Row, Col } from 'react-flexbox-grid';
 import TrainingOptionBox from '../../Components/TrainingOptionBox';
 
 const HomePage = () => {
-    return (
-        <Container>
-            <Row>
-                <Col lg={4} md={6} sm={12}>
-                    <TrainingOptionBox 
-                        testName="Treino de LEITURA e SIGNIFICADO" 
-                        testDescription="Um kanji é sorteado, e devesse determinar quais são suas leituras (kunyomi, onyomi) e qual é seu significado." 
-                        urlTeste="1"
-                    />
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <TrainingOptionBox 
-                        testName="Treino de KANJI e LEITURA (FACIL)"
-                        testDescription="Um significado é sorteado. A partir desse significado deve-se escolher, entre 5 cards, qual kanji e leituras que representam esse significado dado." 
-                        urlTeste="2"
-                    />
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <TrainingOptionBox 
-                        testName="Treino de KANJI e LEITURA (DIFICIL)" 
-                        testDescription="Um significado é sorteado. A partir desse significado é pedido o kanji e as leituras que satisfazem esse significado." 
-                        urlTeste="3"
-                    />
-                </Col>
-                <Col lg={4} md={6} sm={12}>
-                    <TrainingOptionBox 
-                        testName="Treino de KANJI"
-                        testDescription="São dadas as leituras (onyomi e kunyomi) de um kanji, além do seu significado, e é pedido o kanji." 
-                        urlTeste="4"
-                    />
-                </Col>
-            </Row>
-        </Container>
-    );
+  return (
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+    <Grid fluid>
+      <Row>
+        <Col md={4} sm={6}>
+          <TrainingOptionBox
+            testName="Treino de LEITURA e SIGNIFICADO"
+            testDescription="Um kanji é sorteado, e devesse determinar quais são suas leituras (kunyomi, onyomi) e qual é seu significado."
+            urlTeste="1"
+          />
+        </Col>
+        <Col md={4} sm={6}>
+          <TrainingOptionBox
+            testName="Treino de KANJI e LEITURA (FACIL)"
+            testDescription="Um significado é sorteado. A partir desse significado deve-se escolher, entre 5 cards, qual kanji e leituras que representam esse significado dado."
+            urlTeste="2"
+          />
+        </Col>
+        <Col md={4} sm={6}>
+          <TrainingOptionBox
+            testName="Treino de KANJI e LEITURA (DIFICIL)"
+            testDescription="Um significado é sorteado. A partir desse significado é pedido o kanji e as leituras que satisfazem esse significado."
+            urlTeste="3"
+          />
+        </Col>
+        <Col md={4} sm={6}>
+          <TrainingOptionBox
+            testName="Treino de KANJI"
+            testDescription="São dadas as leituras (onyomi e kunyomi) de um kanji, além do seu significado, e é pedido o kanji."
+            urlTeste="4"
+          />
+        </Col>
+      </Row>
+    </Grid>
+    </div>
+  );
 };
 
 export default HomePage;
