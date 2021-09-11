a5dca53c4e23b0b50fae5893f18fd36825036132
nYCSTs <lucascandradex14@gmail.com>
2021-09-11 20:26:05 -0300

    Add Chapters page

---
+++ src/Pages/Chapters/Style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+import { colors } from '../../Constants/colors';
+
+export const ChaptersList = styled.div`
+  background-color: ${colors.background}
+`;
+
+export const Chapter = styled.div`
+  display: flex;
+`;
---
+++ src/Pages/Chapters/index.js
@@ -0,0 +1,16 @@
+import {
+  ChaptersList, Chapter
+} from './Style'
+
+const Chapters = () => {
+  return (
+    <ChaptersList>
+      <Chapter>
+        <h1>Capitulo 1</h1>
+      </Chapter>
+      <hr />
+    </ChaptersList>
+  );
+};
+
+export default Chapters;
