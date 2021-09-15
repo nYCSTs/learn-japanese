bbffd57711628b3fd6a9bcb9df792833135d4cda
nYCSTs <lucascandradex14@gmail.com>
2021-09-14 21:27:12 -0300

    Remove margin that was being used in all pages.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import Routes from './Routes'
 import UserProvider from './Context';
-import AppDiv from './AppDiv';
 
 function App() {
   return (
     <UserProvider>
-      <AppDiv>
-        <Routes />
-      </AppDiv>
+      <Routes />
     </UserProvider>
   );
 }
